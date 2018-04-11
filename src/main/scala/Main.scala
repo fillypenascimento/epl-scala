f68a70fb3a13353a0de705c54eda1a232d834821
fillypenascimento <fillype.n@outlook.com>
2018-04-11 20:41:16 -0300

    commit inicial e final do epl em scala com expressoes simples

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,13 +2,27 @@ import br.unb.cic.epl.Core
 import br.unb.cic.epl.Eval
 import br.unb.cic.epl.Add
 import br.unb.cic.epl.AddEval
+import br.unb.cic.epl.Sub
+import br.unb.cic.epl.SubEval
+import br.unb.cic.epl.Mult
+import br.unb.cic.epl.MultEval
 
 object Main extends App {
-  val lit100 = new Core.Literal(100) with Eval.Literal
-  val lit500 = new Core.Literal(500) with Eval.Literal
+  val lit100 = new Core.Literal((100) + (500)) with Eval.Literal
+  val lit500 = new Core.Literal(400) with Eval.Literal
 
   val sum = new Add.Add(lit100, lit500) 
+  val sub = new Sub.Sub(lit100, lit500)
+  val mult = new Mult.Mult(lit100, lit500)
+
+  val sum_eval = new AddEval.Add(lit100, lit500)
+  val sub_eval = new SubEval.Sub(lit100, lit500)
+  val mult_eval = new MultEval.Mult(lit100, lit500)
 
   println(sum.print())
-  println(lit100.eval())
+  println(sum_eval.eval())
+  println(sub.print())
+  println(sub_eval.eval())
+  println(mult.print())
+  println(mult_eval.eval())
 }
